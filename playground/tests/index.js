bb83c88ea983022573ff7646495dceb666d6fbec
Jacek Góraj <jckgrj@gmail.com>
2018-04-23 12:22:41 +0200

    added test summary and exit code

--- playground/tests/index.js
+++ playground/tests/index.js
@@ -1,2 +1,6 @@
+const { end } = require('allin');
+
 require('./string-utils.test');
 require('./number-utils.test');
+
+end();
--- proj/src/index.js
+++ proj/src/index.js
@@ -1,13 +1,27 @@
 const colors = require('colors');
 
+const summary = { success: 0, fail: 0, ignored: 0};
+
 const check = (description, callback) => {
     try {
         callback();
         console.log(`${' ✓ '.bgGreen.black} ${description.green}`);
+        summary.success++;
     } catch (e) {
         console.log(`${' FAILED '.bgRed.black} ${description.red}`);
         console.log(e.stack.red);
+        summary.fail++;
     }
+};
+
+const end = () => {
+    console.log(`\nTest summary:`);
+    console.log(`   Success: ${summary.success}`.green);
+    console.log(`   Fail: ${summary.fail}`.red);
+    console.log(`   Ignored: ${summary.ignored}`.yellow);
+    console.log('\n');
+    if (summary.fail > 0) process.exit(1);
+    process.exit(0);
 };
 
 const expect = (val) => {
@@ -17,6 +31,7 @@ const expect = (val) => {
 
 const xcheck = (description, callback) => {
     console.log(`${' IGNORED '.bgYellow.black} ${description.yellow}`);
+    summary.ignored++;
 };
 
-module.exports = { check, expect, xcheck };
+module.exports = { check, end, expect, xcheck };
