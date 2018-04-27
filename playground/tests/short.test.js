42c48708449521ddccba66fff0b4b9ca365f1702
Jacek Góraj <jckgrj@gmail.com>
2018-04-27 14:13:42 +0200

    WIP

--- playground/tests/short.test.js
+++ playground/tests/short.test.js
@@ -1,4 +1,5 @@
 const { beforeAll, expect, check, scenario } = require('allin');
+const request = require('request');
 
 scenario('Testing to.be.sameAs', () => {
     
@@ -8,7 +9,11 @@ scenario('Testing to.be.sameAs', () => {
 
     scenario('Just testing before Each', () => {
         check('should to.be.sameAs work - PASSED', () => {
+            request('https://http.cat/200', function (error, response, body) {
+                console.log('2');
+                resolve(response.statusCode);
+            });
             expect(2).to.be.sameAs(2);
         });
-    });
+    }).t;
 });
